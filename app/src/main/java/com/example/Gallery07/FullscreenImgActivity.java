ff7da529cefe165a85c899e458980c98c9798224
TNHao <trannhathao1516@gmail.com>
2021-12-01 23:10:11 +0700

    added edit image

--- app/src/main/java/com/example/Gallery07/FullscreenImgActivity.java
+++ app/src/main/java/com/example/Gallery07/FullscreenImgActivity.java
@@ -6,8 +6,11 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -16,27 +19,43 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 
 import java.io.File;
 
-public class FullscreenImgActivity extends Activity {
+import iamutkarshtiwari.github.io.ananas.editimage.EditImageActivity;
+import iamutkarshtiwari.github.io.ananas.editimage.ImageEditorIntentBuilder;
+
+public class FullscreenImgActivity extends AppCompatActivity {
+    private final int PHOTO_EDITOR_REQUEST_CODE = 231;
+
     private ImageView bigImageView;
+    private Button editButton;
     private Button backButton;
     private Button addToFolderButton;
+    private String curPath;
+
+    ActivityResultLauncher<Intent> editResultLauncher;
     //Avoid Multiple Click On The Same Target
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fullscreen_img);
         bigImageView = findViewById(R.id.bigImageView);
+        editButton = findViewById(R.id.editButton);
         backButton = findViewById(R.id.backButton);
         addToFolderButton = findViewById(R.id.addToFolderButton);
         Intent intent = getIntent();
-        String curPath = intent.getStringExtra("curPath");
+        curPath = intent.getStringExtra("curPath");
+
+        setupActivityResultLaunchers();
+
         Glide.with(getApplicationContext())
                 .load(curPath)
                 .apply(new RequestOptions().centerCrop())
@@ -47,7 +66,62 @@ public class FullscreenImgActivity extends Activity {
                 finish();
             }
         });
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditImage();
+            }
+        });
     }
 
+    private void setupActivityResultLaunchers() {
+        editResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                    }
+                });
+    }
 
+    private void EditImage() {
+        try {
+            Intent intent = new ImageEditorIntentBuilder(this, curPath, curPath)
+                    .withAddText()
+                    .withPaintFeature()
+                    .withFilterFeature()
+                    .withRotateFeature()
+                    .withCropFeature()
+                    .withBrightnessFeature()
+                    .withSaturationFeature()
+                    .withBeautyFeature()
+                    .withStickerFeature()
+                    .forcePortrait(true)
+                    .build();
+            EditImageActivity.start(editResultLauncher, intent, this);
+        } catch (Exception e) {
+//            Log.e("editor eror", e.getMessage());
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            String newFilePath = data.getStringExtra(ImageEditorIntentBuilder.OUTPUT_PATH);
+            boolean isImageEdit = data.getBooleanExtra(EditImageActivity.IS_IMAGE_EDITED, false);
+            System.out.println(newFilePath);
+
+            if (isImageEdit) {
+                File imageFile = new File(newFilePath);
+                if (imageFile.exists()) {
+                    Bitmap image = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+                    bigImageView.setImageBitmap(image);
+                }
+            }
+        }
+    }
 }
